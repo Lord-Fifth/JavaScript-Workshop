c987daf9eddc79cdff067d4d1654155d7720be11
Lord-Fifth <aditya.suresh098@gmail.com>
2019-08-23 14:30:06 +0530

    JS Functions

--- To-Do-App/script.js
+++ To-Do-App/script.js
@@ -1,10 +1,21 @@
 function getInputValue() {
     // console.log('Clicked!')
     const ip = document.getElementById('app-item-to-add');
-    console.log(ip.value)
+    // console.log(ip.value)
+    return ip.value;
+}
+
+function appendToList(toAdd) {
+    const ul = document.getElementById('app-add-list');
+    const newli = document.createElement('li')
+    newli.innerText = toAdd;
+    ul.appendChild(newli);
+    return;
+
 }
 
 const btn = document.getElementById('app-add-btn')
 btn.addEventListener('click', function() {
-    getInputValue()
+    const toInsert = getInputValue()
+    appendToList(toInsert)
 })
